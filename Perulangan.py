1748a9c605f4db67bf32235f35b2a2dc9b48a15c
Alwi950 <alwiakbar950@gmail.com>
2023-05-02 11:24:25 +0700

    Sekuensial perulangan

---
+++ Perulangan.py
@@ -0,0 +1,16 @@
+#Sintaksis_Perulangan
+
+"""
+Program perulangan membaca buku
+"""
+Jumlah_buku = 10
+print('Ibu berkata,"Baca semua bukumu"')
+
+Jumlah_buku_yang_sudah_dibaca= 0
+print(f"jumlah buku yang sudah dibaca {Jumlah_buku_yang_sudah_dibaca}")
+
+for Jumlah_buku_yang_sudah_dibaca in range(1, Jumlah_buku+1):
+    print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(f"Jumlah buku yang sudah dibaca {Jumlah_buku_yang_sudah_dibaca}")
+
